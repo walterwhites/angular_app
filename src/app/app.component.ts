ff12f05140b56592e4f7b0300dbf289591af02ce
flo <hopemagie@gmail.com>
2018-05-02 11:55:55 +0200

    blog angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,9 +14,12 @@ export class AppComponent implements OnInit {
   constructor() {
       this.arrayPost = new ArrayPost();
       this.post_beatles = new Post('Beatles', 'Beatles are the best abnd in the world, why ? Listen them and you' +
-          'will understand', 100000, new Date('1960-01-17T03:24:00'));
-      this.post_stromae = new Post('Stromae', 'Tout simplement Formidable :) ', 100000, new Date('1960-01-17T03:24:00'));
-      this.post_ironMaiden = new Post('Iron Maiden', 'Listen The trooper, Fear of the Dark ... ', 100000, new Date('1960-01-17T03:24:00'));
+          'will understand', 100000, new Date('1960-01-17T03:24:00'),
+          'https://pbs.twimg.com/profile_images/699984147956289536/CjPw79mo_400x400.jpg');
+      this.post_stromae = new Post('Stromae', 'Tout simplement Formidable :) ', 100000, new Date('1960-01-17T03:24:00'),
+          'https://pbs.twimg.com/profile_images/643635163340017664/LK6DJ8eb_400x400.jpg');
+      this.post_ironMaiden = new Post('Iron Maiden', 'Listen The trooper, Fear of the Dark ... ', 100000, new Date('1960-01-17T03:24:00'),
+          'https://c1.accu.fm/static/images/covers256//covers/g-m/ironmaiden_pieceofmindironma.jpg');
       this.arrayPost.addPost(this.post_beatles);
       this.arrayPost.addPost(this.post_stromae);
       this.arrayPost.addPost(this.post_ironMaiden);
@@ -30,16 +33,18 @@ export class Post {
     content: string;
     loveIts: number;
     created_at: Date;
-      constructor(title: string, content: string, loveIts: number, created_at: Date) {
+    picture: String;
+      constructor(title: string, content: string, loveIts: number, created_at: Date, picture: String) {
       this.title = title;
       this.content = content;
       this.loveIts = loveIts;
       this.created_at = created_at;
+      this.picture = picture;
     }
 }
 
 export class ArrayPost {
-    posts: Post[];
+    posts: Post[] = [];
     addPost(element: Post) {
         this.posts.push(element);
     }
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {ArrayPost} from '../app.component';
 
 @Component({
   selector: 'app-post-list',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class PostListComponent implements OnInit {
-    arrayPost = this.arrayPost;
+    @Input() vals: ArrayPost;
     constructor() { }
     ngOnInit() {
     }
